e0cb8a027cf253bc43f0a567efb02d0c84d82389
PRAJAPATI HARSH <harshprajapati8583@gmail.com>
2024-06-18 21:47:01 +0530

    ui

--- src/components/StudyCard.jsx
+++ src/components/StudyCard.jsx
@@ -9,9 +9,8 @@ const Cards = ({ data }) => {
           return (
             <div key={e.id} className="my-3 ">
               {!e.done && (
-                <div className="absolute  text-red-600   text-2xl top-[200px]  select-none -rotate-45 font-mono backdrop-blur-sm  rounded opacity-1 -z-0  ">
-                  {" "}
-                  <h1>UNDER CONSTRUCTION..✨</h1>
+                <div className=" md:absolute relative   text-red-600 text-center flex justify-center top-[180px] md:py-2 py-10 -rotate-60   text-2xl md:text-2xl md:top-[195px]  select-none md:-rotate-45 font-mono backdrop-blur-sm  rounded opacity-1 -z-0  ">
+                  UNDER CONSTRUCTION..✨
                 </div>
               )}
               <div className=" border  p-5 rounded-lg md:h-full md:w-[300px] lg:h-full sm-h-full sm-w-full h-fit w-fit">
@@ -20,20 +19,14 @@ const Cards = ({ data }) => {
 
                 {!e.done && (
                   <NavLink to={e.url} className="my-2 removeLinkHover -z-10">
-                    <button
-                      className="md:px-7 mt-5 md:py-[5px] py-[9px] px-9 m-auto bg-[#29303d] hover:bg-slate-500 rounded"
-                      
-                    >
+                    <button className="md:px-7 mt-5 md:py-[5px] py-[9px] px-9 m-auto bg-[#29303d] hover:bg-slate-500 rounded">
                       Get Code
                     </button>
                   </NavLink>
                 )}
                 {e.done && (
                   <NavLink to={e.url} className="my-2 removeLinkHover">
-                    <button
-                      className="md:px-7 mt-5 md:py-[5px] py-[9px] px-9 m-auto bg-[#29303d] hover:bg-slate-500 rounded"
-                      
-                    >
+                    <button className="md:px-7 mt-5 md:py-[5px] py-[9px] px-9 m-auto bg-[#29303d] hover:bg-slate-500 rounded">
                       Get Code
                     </button>
                   </NavLink>
